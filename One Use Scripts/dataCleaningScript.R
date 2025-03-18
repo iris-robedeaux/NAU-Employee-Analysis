49be5aff58c643a7c8aa7881d37d1cb2982499da
Iris Robedeaux <thebatteryisbroken@gmail.com>
2025-03-17 23:05:12 -0700

    prof demographic data (clean)
    the script and resulting data is here! the IPEDS and regular ethnicity columns were the same, so i dropped one of them, and then I dropped a couple other columns we don't need. also made the salary into a readable number!

---
+++ One Use Scripts/dataCleaningScript.R
@@ -0,0 +1,23 @@
+library(readxl)
+library(dplyr)
+
+#read the original file
+data <- read_excel("Merged_Salary_ProfDemos_Blinded.xlsx")
+
+#remove $ and , from the salary (and make it a number)
+data$`Annual Wages` <- as.numeric(gsub('[$,]', '', data$`Annual Wages`))
+
+#name it to academic year (for accuracy)
+data <- data %>% rename_at('Annual Wages', ~'AY Salary')
+
+#rename our id column properly
+data <- data %>% rename_at('First Name Last Name', ~'Employee ID')
+
+#drop the original employee id, birthday (age is separate), 
+#            employer (all nau), IPEDS Ethnicity (same as 
+#            the enthnicity column), & source
+colsToRemove <- c('Emplid', 'Birth Date', 'Employer', 'IPEDS Ethnicity', 'Source')
+data <- subset(data, select = !(names(data) %in% colsToRemove)) 
+
+#export cleaned data
+write.csv(data, "C:/Users/Iris/Desktop/Capstone/profData.csv",  row.names=FALSE)
